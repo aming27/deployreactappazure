7bfe98772946cb78fad7d9f6379edce2fa53ab84
Erik Asensio <erikasensioperez@gmail.com>
2022-08-22 17:00:56 +0200

    updated the logo and added Kalam font into css

--- deploy/erikApp/app/src/components/Header.jsx
+++ deploy/erikApp/app/src/components/Header.jsx
@@ -2,7 +2,7 @@ import "./Header.css"
 import Menu from "./Menu"
 import hamMenu from "../img/hamMenu.svg"
 
-function Header({onNavHome}) {
+function Header() {
 
     const handleNavHome = () => {
         console.log("handleNavHome")
@@ -10,11 +10,11 @@ function Header({onNavHome}) {
 
     const handleOpenMenu = () => {
         console.log("handleNavMenu");
-        <Menu onNavHome={onNavHome}/>
+        <Menu />
     }
 
     return <div className="header">
-        <a target="_blank" href="http://preview.themeforest.net/item/alison-portfolio-html-template/full_screen_preview/23151102"><img className="logo" src="" alt="Erik Asensio - Full Stack Dev" onClick={handleNavHome} /></a>
+        <a className="logo" target="_blank" rel="noreferrer" href="http://preview.themeforest.net/item/alison-portfolio-html-template/full_screen_preview/23151102" onClick={handleNavHome}>Erik Asensio - Full Stack Dev</a>
         <img className="hamburgerMenu" src={hamMenu} alt="" onClick={handleOpenMenu} />
     </div>
 }

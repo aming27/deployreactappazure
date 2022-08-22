2695ed17af81ca9004743f4804ce78c5e4359822
Erik Asensio <erikasensioperez@gmail.com>
2022-08-22 16:12:46 +0200

    designed and structured all the app's components

---
+++ deploy/erikApp/app/src/components/About.jsx
@@ -0,0 +1,10 @@
+import "./About.css"
+
+function About() {
+
+    return <div className="bgAbout" id="#about">
+        <h1>ABOUT PANEL</h1>
+    </div>
+}
+
+export default About
---
+++ deploy/erikApp/app/src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import "./Hero.css"
+
+function Hero(){
+    return <div className="bgHero">
+        <h1>HERO PANEL</h1>
+        <p>(If you click the logo you will be redirected to the ux/ui template inspiration)</p>
+    </div>
+}
+
+export default Hero
